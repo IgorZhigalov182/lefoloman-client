aff36894a2aab1405461c51c788751848668feda
Geetork <galinaleespb@gmail.com>
2023-10-09 23:28:03 +0300

    branches with mock data added

--- src/components/ui/map/Map.jsx
+++ src/components/ui/map/Map.jsx
@@ -1,6 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import usePosition from '../../../hooks/usePosition';
 import { useEffect } from 'react';
+import data from './data.json';
 
 const Map = () => {
   const { position, error } = usePosition();
@@ -22,16 +23,20 @@ const Map = () => {
     longitude &&
     latitude && (
       <div className="container">
-        <MapContainer center={[latitude, longitude]} zoom={15} scrollWheelZoom={false}>
+        <MapContainer center={[latitude, longitude]} zoom={15} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[latitude, longitude]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          {
+            data?.branch.map(branch => (
+              <Marker position={[...branch.coordinates]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            ))
+          }
         </MapContainer>
       </div>
     )
