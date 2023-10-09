6f147793bfb55feae9741a524bc753b6b7f0b9e1
Geetork <galinaleespb@gmail.com>
2023-10-09 10:39:28 +0300

    map added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
 import '../src/styles/App.css';
 import Map from './components/ui/map/Map';
-import UseActualPosition from './hooks/usePosition';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="container">
+    <div className='container'>
       {/* Navbar */}
       <Map />
     </div>
