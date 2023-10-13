595935803c9dd0f7d7c97e991ec91fd059a92424
Geetork <galinaleespb@gmail.com>
2023-10-14 00:58:34 +0300

    branches sidebar

--- src/components/ui/navbar/Navbar.jsx
+++ src/components/ui/navbar/Navbar.jsx
@@ -1,5 +1,36 @@
+import { useState } from 'react';
+import { CHANGE_NAV_TAB } from '../../../services/actions';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+
+import './Navbar.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+
+const items = [
+  {
+    label: 'Отделения',
+    key: 'branches',
+    icon: <MailOutlined />,
+  },
+  {
+    label: 'История',
+    key: 'history',
+    icon: <AppstoreOutlined />,
+  }
+]
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navTab = useSelector(store => store.state.navTab);
+  const dispatch = useDispatch();
+
+  const onClick = (e) => {
+    dispatch({
+      type: CHANGE_NAV_TAB,
+      navTab: e.key
+    });
+  };
+
+  return <Menu onClick={onClick} selectedKeys={[navTab]} mode="horizontal" items={items} />;
 };
 
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/index.css';
-// import './styles/reset.scss';
-// import './styles/index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './services/reducers';
+
+const store = createStore(reducer);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> 
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
