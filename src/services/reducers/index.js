d930cd9e3150173e8e18abca66900fe146d40716
Igor <zhigalovzenit112233@gmail.com>
2023-10-14 19:54:53 +0300

    add TOGGLE_FOOTER

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,167 +1,182 @@
-import { CHANGE_NAV_TAB, GET_BRANCHES_ERRROR, GET_BRANCHES_REQUEST, GET_BRANCHES_SUCCESS, SEARCH_BRANCH } from '../actions';
+import {
+  CHANGE_NAV_TAB,
+  GET_BRANCHES_ERRROR,
+  GET_BRANCHES_REQUEST,
+  GET_BRANCHES_SUCCESS,
+  SEARCH_BRANCH,
+  TOGGLE_FOOTER,
+} from '../actions';
 import { combineReducers } from 'redux';
 
 const initialState = {
-    navTab: 'branches',
-    branchesRequest: false,
-    error: true,
-    branches: [{
-        "id": 11,
-        "name": "ДО «Королевский» Филиала № 7701 Банка ВТБ (ПАО)",
-        "address": "141070, Московская область, г. Королев, пр-т Космонавтов, д. 33, корп. 1",
-        "rating": null,
-        "latitude": 55.910648,
-        "longitude": 37.867706,
-        "departments": [
+  navTab: 'branches',
+  branchesRequest: false,
+  error: true,
+  footer: false,
+  branches: [
+    {
+      id: 11,
+      name: 'ДО «Королевский» Филиала № 7701 Банка ВТБ (ПАО)',
+      address: '141070, Московская область, г. Королев, пр-т Космонавтов, д. 33, корп. 1',
+      rating: null,
+      latitude: 55.910648,
+      longitude: 37.867706,
+      departments: [
+        {
+          name: 'openHoursIndividual',
+          open_hours: [
             {
-                "name": "openHoursIndividual",
-                "open_hours": [
-                    {
-                        "openAt": "09:00",
-                        "closedAt": "18:00",
-                        "dayOfWeek": "ПОНЕДЕЛЬНИК"
-                    },
-                    {
-                        "openAt": "09:00",
-                        "closedAt": "18:00",
-                        "dayOfWeek": "ВТОРНИК"
-                    },
-                    {
-                        "openAt": "09:00",
-                        "closedAt": "18:00",
-                        "dayOfWeek": "СРЕДА"
-                    },
-                    {
-                        "openAt": "09:00",
-                        "closedAt": "18:00",
-                        "dayOfWeek": "ЧЕТВЕРГ"
-                    },
-                    {
-                        "openAt": "09:00",
-                        "closedAt": "18:00",
-                        "dayOfWeek": "ПЯТНИЦА"
-                    },
-                    {
-                        "openAt": "0:00",
-                        "closedAt": "0:00",
-                        "dayOfWeek": "СУББОТА"
-                    },
-                    {
-                        "openAt": "0:00",
-                        "closedAt": "0:00",
-                        "dayOfWeek": "ВОСКРЕСЕНИЕ"
-                    }
-                ]
-            }
-        ],
-        "sale_point_format": null,
-        "metro_station": null,
-        "phone_number": null
+              openAt: '09:00',
+              closedAt: '18:00',
+              dayOfWeek: 'ПОНЕДЕЛЬНИК',
+            },
+            {
+              openAt: '09:00',
+              closedAt: '18:00',
+              dayOfWeek: 'ВТОРНИК',
+            },
+            {
+              openAt: '09:00',
+              closedAt: '18:00',
+              dayOfWeek: 'СРЕДА',
+            },
+            {
+              openAt: '09:00',
+              closedAt: '18:00',
+              dayOfWeek: 'ЧЕТВЕРГ',
+            },
+            {
+              openAt: '09:00',
+              closedAt: '18:00',
+              dayOfWeek: 'ПЯТНИЦА',
+            },
+            {
+              openAt: '0:00',
+              closedAt: '0:00',
+              dayOfWeek: 'СУББОТА',
+            },
+            {
+              openAt: '0:00',
+              closedAt: '0:00',
+              dayOfWeek: 'ВОСКРЕСЕНИЕ',
+            },
+          ],
+        },
+      ],
+      sale_point_format: null,
+      metro_station: null,
+      phone_number: null,
     },
     {
-        "id": 11,
-        "name": "ДО «Королевский» Филиала № 7701 Банка ВТБ (ПАО)",
-        "address": "141070, Московская область, г. Королев, пр-т Космонавтов, д. 33, корп. 1",
-        "rating": null,
-        "latitude": 55.910648,
-        "longitude": 37.867706,
-        "departments": [
+      id: 11,
+      name: 'ДО «Королевский» Филиала № 7701 Банка ВТБ (ПАО)',
+      address: '141070, Московская область, г. Королев, пр-т Космонавтов, д. 33, корп. 1',
+      rating: null,
+      latitude: 55.910648,
+      longitude: 37.867706,
+      departments: [
+        {
+          name: 'openHoursIndividual',
+          open_hours: [
             {
-                "name": "openHoursIndividual",
-                "open_hours": [
-                    {
-                        "openAt": "09:00",
-                        "closedAt": "18:00",
-                        "dayOfWeek": "ПОНЕДЕЛЬНИК"
-                    },
-                    {
-                        "openAt": "09:00",
-                        "closedAt": "18:00",
-                        "dayOfWeek": "ВТОРНИК"
-                    },
-                    {
-                        "openAt": "09:00",
-                        "closedAt": "18:00",
-                        "dayOfWeek": "СРЕДА"
-                    },
-                    {
-                        "openAt": "09:00",
-                        "closedAt": "18:00",
-                        "dayOfWeek": "ЧЕТВЕРГ"
-                    },
-                    {
-                        "openAt": "09:00",
-                        "closedAt": "18:00",
-                        "dayOfWeek": "ПЯТНИЦА"
-                    },
-                    {
-                        "openAt": "0:00",
-                        "closedAt": "0:00",
-                        "dayOfWeek": "СУББОТА"
-                    },
-                    {
-                        "openAt": "0:00",
-                        "closedAt": "0:00",
-                        "dayOfWeek": "ВОСКРЕСЕНИЕ"
-                    }
-                ]
-            }
-        ],
-        "sale_point_format": null,
-        "metro_station": null,
-        "phone_number": null
-    }
-    ],
-}
+              openAt: '09:00',
+              closedAt: '18:00',
+              dayOfWeek: 'ПОНЕДЕЛЬНИК',
+            },
+            {
+              openAt: '09:00',
+              closedAt: '18:00',
+              dayOfWeek: 'ВТОРНИК',
+            },
+            {
+              openAt: '09:00',
+              closedAt: '18:00',
+              dayOfWeek: 'СРЕДА',
+            },
+            {
+              openAt: '09:00',
+              closedAt: '18:00',
+              dayOfWeek: 'ЧЕТВЕРГ',
+            },
+            {
+              openAt: '09:00',
+              closedAt: '18:00',
+              dayOfWeek: 'ПЯТНИЦА',
+            },
+            {
+              openAt: '0:00',
+              closedAt: '0:00',
+              dayOfWeek: 'СУББОТА',
+            },
+            {
+              openAt: '0:00',
+              closedAt: '0:00',
+              dayOfWeek: 'ВОСКРЕСЕНИЕ',
+            },
+          ],
+        },
+      ],
+      sale_point_format: null,
+      metro_station: null,
+      phone_number: null,
+    },
+  ],
+};
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case CHANGE_NAV_TAB: {
-            return {
-                ...state,
-                navTab: action.navTab
-            }
-        }
-
-        case GET_BRANCHES_REQUEST: {
-            return {
-                ...state,
-                branchesRequest: true
-            }
-        }
-
-        case GET_BRANCHES_ERRROR: {
-            return {
-                ...state,
-                branchesRequest: false,
-                error: true
-            }
-        }
-
-        case GET_BRANCHES_SUCCESS: {
-            return {
-                ...state,
-                branchesRequest: false,
-                branches: action.branches
-            }
-        }
-
-        case SEARCH_BRANCH: {
-            const data = state.branches;
-            return {
-                ...state,
-                branches: data
-            }
-        }
-
-        default: {
-            return state;
-        }
+  switch (action.type) {
+    case CHANGE_NAV_TAB: {
+      return {
+        ...state,
+        navTab: action.navTab,
+      };
     }
-}
 
+    case GET_BRANCHES_REQUEST: {
+      return {
+        ...state,
+        branchesRequest: true,
+      };
+    }
+
+    case GET_BRANCHES_ERRROR: {
+      return {
+        ...state,
+        branchesRequest: false,
+        error: true,
+      };
+    }
+
+    case GET_BRANCHES_SUCCESS: {
+      return {
+        ...state,
+        branchesRequest: false,
+        branches: action.branches,
+      };
+    }
+
+    case SEARCH_BRANCH: {
+      const data = state.branches;
+      return {
+        ...state,
+        branches: data,
+      };
+    }
+
+    case TOGGLE_FOOTER: {
+      return {
+        ...state,
+        footer: !state.footer,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
 
 const rootReducer = combineReducers({
-    state: reducer,
+  state: reducer,
 });
 export default rootReducer;
